79fa77831ece1eca2fabc0b0026bf5a4eb8d157e
Felipe Araujo <felipefagyn@gmail.com>
2019-06-04 16:21:05 -0300

    Criação de formulário para cadastro de tipo de fornecedor

---
+++ tcc-totvs/forms/Cadastro de Tipo de Fornecedor/events/validateForm.js
@@ -0,0 +1,23 @@
+function validateForm(form) {
+	var campos = '';
+
+	if (form.getValue('ramoAtividade') == '') {
+		campos += '<br>' + i18n.translate('ramoAtividade');
+	}
+	if (form.getValue('possuiLimite') == '' || form.getValue('possuiLimite') == null) {
+		campos += '<br>' + i18n.translate('possuiLimite');
+	} else if (form.getValue('possuiLimite') == 'sim') {
+		if (form.getValue('valorLimite') == '') {
+			campos += '<br>' + i18n.translate('valorLimite');
+		}
+	}
+
+	if (campos != '') {
+		var cabecalhoMsg = i18n.translate('msgCampoVazio');
+		var quantidadeCampos = campos.split('<br>').length - 1; // -1 para remover posição do array com string vazia
+		if (quantidadeCampos > 1) {
+			cabecalhoMsg = i18n.translate('msgCamposVazios');
+		}
+		throw cabecalhoMsg + '<br>' + campos;
+	}
+}
---
+++ tcc-totvs/forms/Cadastro de Tipo de Fornecedor/view.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+	$(':text, select').on('blur', function () {
+		let input = $(this);
+		if (input.val() == '' || input.val() == null) {
+			input.parent().addClass('has-error');
+		} else {
+			input.parent().removeClass('has-error');
+		}
+	});
+
+	$('#possuiLimite').on('change', function () {
+		let possuiLimite = $(this).children('option:selected').val();
+		if (possuiLimite === 'sim') {
+			$('.valorLimite').show();
+		} else {
+			$('.valorLimite').hide();
+			$('#valorLimite').val('');
+		}
+	});
+
+	$('.real').maskMoney({
+		prefixMoney: 'R$ ',
+		placeholder: 'R$ 0,00'
+	});
+});
