4bcb79b0e4ad0357c0caef9efed8e44d0a33540b
Felipe Araujo <felipefagyn@gmail.com>
2019-06-05 10:46:35 -0300

    Melhorias na validação visual do cadastro de empresa

--- tcc-totvs/forms/Cadastro de Empresa/scripts.js
+++ tcc-totvs/forms/Cadastro de Empresa/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-	// $(':text').css('text-transform', 'uppercase');
-	$(':text').on('blur', function () {
+	$('.obrigatorio').on('blur', function () {
 		input = $(this);
 		if (input.val() == '' || input.val() == null) {
 			input.parent().addClass('has-error');
@@ -13,9 +12,12 @@ $(document).ready(function () {
 				input.parent().addClass('has-error');
 			}
 		} else {
-			// input.val(input.val().toUpperCase());
 			input.parent().removeClass('has-error');
 		}
+	});
+
+	$('#cep').on('blur', function () {
+		consultarCep($(this).val());
 	});
 });
 
@@ -27,7 +29,7 @@ $(document).ready(function () {
  * @returns {Boolean} True se cnpj for válido e não estiver cadastrado.
  */
 function validarCnpj(cnpj) {
-	var cnpjComMascara = cnpj;
+	let cnpjComMascara = cnpj;
 	cnpj = cnpj.replace(/[^\d]+/g, '');
 
 	if (cnpj == '') return false;
@@ -35,16 +37,16 @@ function validarCnpj(cnpj) {
 	if (cnpj.length != 14) return false;
 
 	// Elimina CNPJs inválidos conhecidos
-	if (cnpj == "00000000000000" ||
-		cnpj == "11111111111111" ||
-		cnpj == "22222222222222" ||
-		cnpj == "33333333333333" ||
-		cnpj == "44444444444444" ||
-		cnpj == "55555555555555" ||
-		cnpj == "66666666666666" ||
-		cnpj == "77777777777777" ||
-		cnpj == "88888888888888" ||
-		cnpj == "99999999999999")
+	if (cnpj == '00000000000000' ||
+		cnpj == '11111111111111' ||
+		cnpj == '22222222222222' ||
+		cnpj == '33333333333333' ||
+		cnpj == '44444444444444' ||
+		cnpj == '55555555555555' ||
+		cnpj == '66666666666666' ||
+		cnpj == '77777777777777' ||
+		cnpj == '88888888888888' ||
+		cnpj == '99999999999999')
 		return false;
 
 	// Valida dígitos verificadores
@@ -76,10 +78,10 @@ function validarCnpj(cnpj) {
 		return false;
 
 	// Verifica se CNPJ já está cadastrado para outra empresa
-	var constraintsEmpresa = new Array(
+	let constraintsEmpresa = new Array(
 		DatasetFactory.createConstraint('cnpj', cnpjComMascara, cnpjComMascara, ConstraintType.MUST)
 	);
-	var felipe_CadastroEmpresa = DatasetFactory.getDataset('felipe_CadastroEmpresa', null, constraintsEmpresa, null);
+	let felipe_CadastroEmpresa = DatasetFactory.getDataset('felipe_CadastroEmpresa', null, constraintsEmpresa, null);
 
 	if (felipe_CadastroEmpresa.values.length > 0) {
 		// TO DO: melhorar alerta de CNPJ existente
@@ -88,4 +90,66 @@ function validarCnpj(cnpj) {
 	}
 
 	return true;
+}
+
+/**
+ * @function limparEndereco Limpa valores dos campos de endereço.
+ */
+function limparEndereco() {
+	$('#logradouro').val('');
+	$('#bairro').val('');
+	$('#localidade').val('');
+	$('#uf').val('');
+}
+
+/**
+ * @function consultarCep Busca um CEP informado para preencher os demais campos do endereço.
+ * 
+ * @param {String} cep CEP a ser consultado.
+ */
+function consultarCep(cep) {
+	//Verifica se campo cep possui valor informado.
+	if (cep != '') {
+
+		//Nova variável 'cep' somente com dígitos.
+		cep = cep.replace(/\D/g, '');
+
+		//Expressão regular para validar o CEP.
+		let validacep = /^[0-9]{8}$/;
+
+		//Valida o formato do CEP.
+		if (validacep.test(cep)) {
+
+			//Preenche os campos com '...' enquanto consulta webservice.
+			$('#logradouro').val('...');
+			$('#bairro').val('...');
+			$('#localidade').val('...');
+			$('#uf').val('...');
+
+			//Consulta o webservice viacep.com.br/
+			$.getJSON('https://viacep.com.br/ws/' + cep + '/json/?callback=?', function (dados) {
+				if (!('erro' in dados)) {
+					//Atualiza os campos com os valores da consulta.
+					$('#logradouro').val(dados.logradouro);
+					$('#bairro').val(dados.bairro);
+					$('#localidade').val(dados.localidade);
+					$('#uf').val(dados.uf);
+				} //end if.
+				else {
+					//CEP pesquisado não foi encontrado.
+					limparEndereco();
+					alert('CEP não encontrado.');
+				}
+			});
+		} //end if.
+		else {
+			//cep é inválido.
+			limparEndereco();
+			alert('Formato de CEP inválido.');
+		}
+	} //end if.
+	else {
+		//cep sem valor, limpa formulário.
+		limparEndereco();
+	}
 }
