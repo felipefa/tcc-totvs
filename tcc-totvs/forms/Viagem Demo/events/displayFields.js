d65026c122637cc9d3a84ba77b835177ee1ca36e
Felipe Araujo <felipefagyn@gmail.com>
2019-06-12 20:26:46 -0300

    Alteração do nome da pasta do formulário do processo de viagem

--- tcc-totvs/forms/Viagem Demo/events/displayFields.js
+++
@@ -1,4 +0,0 @@
-function displayFields(form, customHTML) {
-	customHTML.append('<script>var codigoAtividade = ' + getValue('WKNumState') + ';</script>');
-	form.setValue('numeroSolicitacao', getValue('WKNumProces'));
-}
